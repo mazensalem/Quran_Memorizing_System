865ee4bac4c526be88f8991d76ab7968826bc4b7
Calculus-savage <abdomao12341@gmail.com>
2025-12-05 16:28:26 +0200

    added Performance_Review page and Book_Sessions page

---
+++ Quran_Memorizing_System/Pages/Book_Sessions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quran_Memorizing_System.Pages
+{
+    public class Book_SessionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Quran_Memorizing_System/Pages/Performace_Review.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quran_Memorizing_System.Pages
+{
+    public class Performace_ReviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
