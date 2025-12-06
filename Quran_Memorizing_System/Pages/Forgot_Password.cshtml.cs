0dbf35bd5c34d9b1de5a90a55bfa9b68e7f396bb
Sayed Numan - 202400861 <202400861@zewailcity.edu.eg>
2025-12-06 18:26:02 +0200

    Forgot and profile Sayed Numan

---
+++ Quran_Memorizing_System/Pages/Forgot_Password.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quran_Memorizing_System.Pages
+{
+    public class Forgot_PasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Quran_Memorizing_System/Pages/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quran_Memorizing_System.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
